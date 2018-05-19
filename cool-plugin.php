fdbe86333e85be67733fff32e451af59c60a08b6
Arek <it.arekko@gmail.com>
2018-05-19 17:24:38 +0200

    added h3

--- cool-plugin.php
+++ cool-plugin.php
@@ -1,5 +1,3 @@
 <?php 
-
-//last brew install zsh-syntax-highlightings
-//something second to commit
+<h3>Nagłowek</h3>
 // last https://www.youtube.com/watch?v=BQzpfloda3M&index=2&list=PLriKzYyLb28nCh3jJLROcYBvj7ZO0l-3G
